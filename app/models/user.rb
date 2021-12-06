e3ed59a2af5752b662be0b65134c51e124db009b
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-06 10:09:51 +0200

    update user model logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_make :likes, dependent: :destroy
+
+  def recent_posts
+    posts.last(5)
+  end
 end
