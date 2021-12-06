ba61787464a001532edb5ed3bcaf9c1fe1afd33b
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-06 10:09:26 +0200

    update posts model logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  belongs_to :user, counter_cache: :posts_count
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_count(count)
+    user.update('posts_count', count)
+  end
 end
