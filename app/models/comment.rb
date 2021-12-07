43ee9380bf906272dd47e5fc8dc216fb218b2d85
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-07 11:06:04 +0200

    modified

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post, counter_cache: :comments_counter
 
   def update_count(count)
-    post.uodate('comments_counter', count)
+    post.update('comments_counter', count)
   end
 end
