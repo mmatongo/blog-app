4a5a28566c163c1eec0960ec0e2d02f70f5674e4
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-08 20:10:03 +0200

    fix linter error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 250, too_long: '%{count} characters is the maximum allowed' }
+  validates :title, presence: true, length: { maximum: 250, too_long: '%<count>s characters is the maximum allowed' }
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   belongs_to :user, counter_cache: :posts_counter
