5f8ccc728f98d7c76dc8f51cafa5b4217fd7bd70
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-08 19:52:27 +0200

    add specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { User.new name: 'Daniel Matongo' }
+
+    before { subject.save }
+
+    it 'return a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have posts > 0' do
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'recent posts' do
+    subject { User.first }
+
+    it 'return 3 posts' do
+      expect(subject.recent_posts.length).to eq(3)
+    end
+  end
 end
