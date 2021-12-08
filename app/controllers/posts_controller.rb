e2530b8e5cc94e4151e9c41de51bebc6fe2878da
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-08 14:12:04 +0200

    update controller logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,30 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
-    @comments = @post.comments.all
+    @comments = @post.comments.all.order('created_at')
+    @liked = @post.liked?(current_user)
+  end
+
+  def new; end
+
+  def create
+    post = current_user.posts.new(post_params)
+
+    respond_to do |format|
+      format.html do
+        if post.save
+          redirect_to user_posts_path(post.user.id, post.id), notice: 'Post was successfully created.'
+        else
+          flash[:alert] = 'Post was not created.'
+          render :new
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
