58fdf791993974de8ae4641a557967e56c8dd8dd
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-13 17:22:42 +0200

    update model logic

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: :likes_counter
+  validates :user_id, uniqueness: { scope: :post_id }
 
   def update_count(count)
     post.update(likes_counter: count)
